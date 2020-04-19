d50ffa7d22ebb055a5c736bcb11242fd77b1d10e
Seekaplayer <44986525+seekaplayer@users.noreply.github.com>
2020-04-19 01:37:31 -0400

    Added in a navigation link item

--- src/components/templates/header/Navigation.js
+++ src/components/templates/header/Navigation.js
@@ -2,6 +2,7 @@ const base = "https://seekaplayer.com";
 export default [
   { name: "Home", target: "_self", url: `${base}` },
   { name: "About", target: "_self", url: `${base}/about` },
+  { name: "Services", target: "_self", url: `${base}/services` },
   { name: "Portfolio", target: "_self", url: "/" },
   { name: "Twitch", target: "_blank", url: "https://twitch.tv/seekaplayer" },
   { name: "Blog", target: "_self", url: `${base}/blog` },
